2a8cb11fdd9ed7f1e6eb16e0f518d8c672d3a6d4
Ilya Pasternak <pasterzoom@gmail.com>
2022-04-01 14:21:49 +0300

    01.04.2022

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Todolist} from "./Todolist";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const arr1=[
+        {id: 1, title: "HTML&CSS11", isDone: true},
+        {id: 2, title: "JS11", isDone: true},
+        {id: 3, title: "ReactJS11", isDone: false},
+        {id: 4, title: "Rest API11", isDone: false},
+    ]
+
+    const arr2=[
+        {id: 1, title: "HTML&CSS222", isDone: true},
+        {id: 2, title: "JS222", isDone: true},
+        {id: 3, title: "ReactJS2222", isDone: false},
+        {id: 4, title: "Rest API22222", isDone: false},
+        {id: 5, title: "GraphQL22222", isDone: false},
+    ]
+
+    return (
+        <div className="App">
+            <Todolist topic1={'What to learn111'} arr={arr1}/>
+            <Todolist topic1={'What to learn222'} arr={arr2}/>
+        </div>
+    );
 }
 
 export default App;

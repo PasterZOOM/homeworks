75f875c459d13b9b3c2dcd3610ecb72dad76f37f
Ilya Pasternak <pasterzoom@gmail.com>
2022-04-30 00:59:02 +0300

    30.04.2022

--- src/p2-homeworks/h5-rrd-v6/Header.tsx
+++ src/p2-homeworks/h5-rrd-v6/Header.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
+import {PATH} from './Pages';
 
 function Header() {
     return (
         <div>
-            // add NavLinks
 
+            <NavLink to={PATH.PRE_JUNIOR} >PreJunior</NavLink>
+            <NavLink to={PATH.JUNIOR} >Junior</NavLink>
+            <NavLink to={PATH['JUNIOR+']} >Junior+</NavLink>
         </div>
     )
 }
--- src/p2-homeworks/h5-rrd-v6/Pages.tsx
+++ src/p2-homeworks/h5-rrd-v6/Pages.tsx
@@ -1,7 +1,12 @@
 import React from 'react'
+import {Route, Routes, Navigate} from 'react-router-dom';
+import PreJunior from './pages/PreJunior';
+import Error404 from './pages/Error404';
 
 export const PATH = {
     PRE_JUNIOR: '/pre-junior',
+    JUNIOR: '/junior',
+    'JUNIOR+': '/junior+'
     // add paths
 }
 
@@ -9,18 +14,16 @@ function Pages() {
     return (
         <div>
             {/*Routes выбирает первый подходящий роут*/}
-            {/*<Routes>*/}
-
+            <Routes>
                 {/*в начале мы попадаем на страницу '/' и переходим сразу на страницу PRE_JUNIOR*/}
-                {/*<Route path={'/'} element={<Navigate to={PATH.PRE_JUNIOR}/>}/>*/}
-
-                {/*<Route path={PATH.PRE_JUNIOR} element={<PreJunior/>}/>*/}
-                // add routes
-
+                <Route path={'/'} element={<Navigate to={PATH.PRE_JUNIOR}/>}/>
+                <Route path={PATH.PRE_JUNIOR} element={<PreJunior/>}/>
+                <Route path={PATH.JUNIOR} element={<Error404/>}/>
+                <Route path={PATH['JUNIOR+']} element={<Error404/>}/>
+                {/*// add routes*/}
                 {/*он отрисуется если пользователь захочет попасть на несуществующую страницу*/}
-                {/*<Route path={'/*'} element={<Error404/>}/>*/}
-
-            {/*</Routes>*/}
+                <Route path={'/*'} element={<Error404/>}/>
+            </Routes>
         </div>
     )
 }
